be2142642830ecd41ee5770851026de03eb4aab7
preeti yadav <preeti@appflowsolutions.com>
2019-07-26 12:50:23 +0530

    menu file changes

--- PMWorks/Pages/MenuPage.xaml.cs
+++ PMWorks/Pages/MenuPage.xaml.cs
@@ -6,19 +6,24 @@ namespace PMWorks.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MenuPage : ContentPage
-	{
-		public MenuPage (string name)
-		{
-			InitializeComponent ();
-            hotelName.Text = name;            
-		}
+    {
+        public MenuPage(string name)
+        {
+            InitializeComponent();
+            hotelName.Text = name;
+        }
 
 
         public async void Reports_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new HotelDetails(hotelName.Text));
         }
-        
+
+        public async void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ConsoleMenuPage());
+        }
+
         public async void Progress_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ProgressScreen(hotelName.Text));
